b7594bc6c620584c36637982094dbeceab6da009
Rudolph <52330699+RudolphLamp@users.noreply.github.com>
2024-05-22 12:31:55 +0200

    Create view.php

---
+++ Task5/view.php
@@ -0,0 +1,45 @@
+<?php
+include 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="css/view.css">
+</head>
+    <div id="flex-container">
+      
+  </div>
+
+    <div class="loader">
+      <img src="img/loader.gif" class="loader" alt="Loading...">
+  </div>
+
+
+
+  <div class="flex-container1">
+    <div id = "HouseView-container" class="HouseView-container">
+      <!-- Placeholder for carosel details -->
+
+    </div>
+  </div>
+        
+        <div class="allinfo">
+            <div class="singleinfo">
+                <div class="details">
+                    <!-- Placeholder for listing details -->
+                </div>
+            </div>
+        </div>
+   
+
+
+
+    <script src="View.js"></script>
+</body>
+</html>
+
+
+<?php
+include 'footer.php';
+?>
