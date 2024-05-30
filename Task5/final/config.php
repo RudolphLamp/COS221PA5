ff5110c8025391dedeb6741d75e4464101463ae1
Rudolph <52330699+RudolphLamp@users.noreply.github.com>
2024-05-30 08:56:05 +0200

    Update config.php

--- Task5/final/config.php
+++ Task5/final/config.php
@@ -3,6 +3,6 @@
 // Define database connection constants
 define("DB_HOST", "wheatley.cs.up.ac.za"); 
 define("DB_USER", "u23536013"); 
-define("DB_PASSWORD", "CB5XXQPCTLSTGWLHVKFOWOET6R6W2UXC"); 
+define("DB_PASSWORD", ""); 
 define("DB_NAME", "u23536013_final_prac_5_database"); 
 ?>
